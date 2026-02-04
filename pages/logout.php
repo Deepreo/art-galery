ab3a044d321c5fe6e3dee3c419bbe0fbc5552368
Yusuf Taha Çimen <yusuftahacimen166@gmail.com>
2026-02-04 18:45:43 +0300

    Add files via upload

---
+++ pages/logout.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+// Prevent back button cache issues
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+// Destroy all session data
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+session_destroy();
+header('Location: login.php');
+exit();
+?>
